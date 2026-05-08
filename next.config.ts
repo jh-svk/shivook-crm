97d69b350b567c94f12a73b9df9ebb6fbb3b315c
Jacob Elbaum <jacob@shivook.com>
2026-05-08 11:54:56 +0300

    fix: use Next 14 serverComponentsExternalPackages, remove stale @types/node-cron

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@prisma/client'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 }
 
 export default nextConfig
